bd862463a4a265ca74ebad269c5b7a6032d25bcb
Luis <account@luishauenstein.com>
2023-01-25 21:16:51 +0100

    deploy script

---
+++ script/MutableNFT.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@forge-std/Script.sol";
+import "../src/MutableNFT.sol";
+
+contract MyScript is Script {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        //
+        // TOKEN METADATA
+        //
+
+        string memory name = "Luis' Mutable NFTs";
+        string memory symbol = "MUTALUIS";
+        string memory baseURI = "https://nft.luish.xyz/";
+        address owner = 0xA7695a6Fb16B0d20d7aA4Eb6615ED86f33c3e01a;
+        
+        vm.startBroadcast(deployerPrivateKey);
+        
+        MutableNFT nft = new MutableNFT(name, symbol, baseURI);
+        nft.transferOwnership(owner);
+        
+        vm.stopBroadcast();
+    }
+}
