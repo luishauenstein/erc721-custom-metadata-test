ee34bd343817900ec821bd720fc210d863ee9c6d
Luis <account@luishauenstein.com>
2023-01-25 20:23:02 +0100

    contract implementation

---
+++ src/MutableNFT.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@oz-contracts/access/Ownable.sol";
+import "@solmate/tokens/ERC721.sol";
+import "@oz-contracts/utils/Strings.sol";
+
+contract MutableNFT is Ownable, ERC721 {
+    
+    using Strings for uint256;
+    string public baseURI;
+    uint256 public currentTokenId;
+    
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory baseURI_
+    ) ERC721 (name_, symbol_){
+        baseURI = baseURI_;
+    }
+
+    //
+    // TOKEN URI
+    //
+
+    function tokenURI(uint256 tokenId)
+    public
+    view
+    override
+    returns (string memory)
+    {
+        return
+        bytes(baseURI).length > 0
+        ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
+        : "";
+    }
+
+    function setBaseURI (string memory newBaseURI_) public onlyOwner returns (string memory){
+        baseURI = newBaseURI_;
+        return baseURI;
+    }
+
+    //
+    // MINT
+    //
+
+    function mintTo(address recipient) public onlyOwner returns (uint256) {
+        uint256 newTokenId = ++currentTokenId;
+        _safeMint(recipient, newTokenId);
+        return newTokenId;
+    }
+}
--- test/MutableNFT.t.sol
+++ test/MutableNFT.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "@forge-std/Test.sol";
+import "../src/MutableNFT.sol";
 
 contract CounterTest is Test {
     Counter public counter;
