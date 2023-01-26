4e9202c1512236011128bf37a10a53226490dc0b
Luis <account@luishauenstein.com>
2023-01-26 17:56:52 +0100

    update script

--- script/MutableNFT.s.sol
+++ script/MutableNFT.s.sol
@@ -8,17 +8,13 @@ contract MyScript is Script {
 
     function run() external {
 
-        //
-        // TOKEN METADATA
-        //
-
         string memory name = "Luis' Mutable NFTs";
         string memory symbol = "MUTALUIS";
         string memory baseURI = "https://nft.luish.xyz/";
-        address owner = 0xA7695a6Fb16B0d20d7aA4Eb6615ED86f33c3e01a;
+        address owner = 0xeFfF014686EAff6406dEAd423511fA608e1CBfe8;
         
         vm.startBroadcast(owner);
-        MutableNFT nft = new MutableNFT(name, symbol, baseURI);  
+        new MutableNFT(name, symbol, baseURI);  
         vm.stopBroadcast();
     }
 }
